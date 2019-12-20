7da9d199685ad47043598cf39a111c8451bf7a79
mikola-s <suharev.nv@gmail.com>
2019-12-20 12:48:44 +0200

    lsn34 skeleton complete

--- lsn34_home_work/scetch/views.py
+++ lsn34_home_work/scetch/views.py
@@ -1,25 +1,26 @@
 from django.shortcuts import render
-
+from random import randint, choice
+from string import digits, ascii_letters
 
 # Create your views here.
 
 def index(request):
-    int_page = 100
-    str_page = 'qwerty'
+    int_page = randint(100, 10000)
+    str_page = ''.join(choice(digits+ascii_letters) for i in range(randint(5, 12)))
     return render(request, 'index.html', {
         'int_page': int_page,
         'str_page': str_page,
     })
 
 
-def int_url(request, page):  # , *args, **kwargs):  # , int_url_val=100):
+def int_url(request, page):
     print(page)
     return render(request, 'int_url.html', {
         'page': page
     })
 
 
-def str_url(request, page):  # , str_url_val='qwerty'):
+def str_url(request, page):
     print(page)
     return render(request, 'str_url.html', {
         'page': page
