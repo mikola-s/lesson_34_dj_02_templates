139bc859370a7c1e4b258d76f89a4d37c74ff998
mikola-s <suharev.nv@gmail.com>
2019-12-20 14:26:56 +0200

    lsn34 complete

--- lsn34_home_work/scetch/urls.py
+++ lsn34_home_work/scetch/urls.py
@@ -4,9 +4,6 @@ from . import views
 
 urlpatterns = [
     path("", views.index, name='index'),
-    # path("100/", views.int_url, name='int_url'),
     path("<int:page>/", views.int_url, name='int_url'),
     path("<str:page>/", views.str_url, name='str_url'),
-    # path("<str:str_url_val>/", views.str_url, name='str_url'),
-
 ]
--- lsn34_home_work/scetch/views.py
+++ lsn34_home_work/scetch/views.py
@@ -2,11 +2,10 @@ from django.shortcuts import render
 from random import randint, choice
 from string import digits, ascii_letters
 
-# Create your views here.
 
 def index(request):
     int_page = randint(100, 10000)
-    str_page = ''.join(choice(digits+ascii_letters) for i in range(randint(5, 12)))
+    str_page = ''.join(choice(digits + ascii_letters) for i in range(randint(5, 12)))
     return render(request, 'index.html', {
         'int_page': int_page,
         'str_page': str_page,
@@ -14,14 +13,12 @@ def index(request):
 
 
 def int_url(request, page):
-    print(page)
     return render(request, 'int_url.html', {
         'page': page
     })
 
 
 def str_url(request, page):
-    print(page)
     return render(request, 'str_url.html', {
         'page': page
     })
