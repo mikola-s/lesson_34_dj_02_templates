6a141eaa0bc6a9e654dc90f30fb66174f6323a2c
mikola-s <suharev.nv@gmail.com>
2019-12-24 16:21:08 +0200

    lsn34 anything changes

--- lsn34_home_work/scetch/models.py
+++ lsn34_home_work/scetch/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
 # Create your models here.
+
+from django.db import models
+
+
+class Test_Model(models.Model):
+    title = models.CharField()
+    email = models.EmailField()
+    publication_date = models.DateField()
